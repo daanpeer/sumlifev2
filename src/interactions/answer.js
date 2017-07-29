0dd3a8f83ade1c2147e48d56d9617db7d94cc261
Daan Houweling <daan.gdh@gmail.com>
2017-07-29 18:06:36 +0200

    Fixed bug where wrong reply was given

--- src/interactions/answer.js
+++ src/interactions/answer.js
@@ -7,9 +7,9 @@ const answer = async (ctx) => {
   const [answer, questionId, userId] = ctx.match[2].split(':')
   if (!(await isAnsweredToday(questionId, userId))) {
     await storeAnswerByToday(questionId, answer, userId)
-    await ctx.reply('I\'ve registered your answer')
+    return ctx.reply('I\'ve registered your answer')
   }
-  await ctx.reply('You\'ve already answered this question')
+  return ctx.reply('You\'ve already answered this question')
 }
 
 export default answer
