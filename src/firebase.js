b18bed17203d395b4a39227e494e3b909008bed0
Daan Houweling <daan.gdh@gmail.com>
2017-08-26 14:41:13 +0200

    Enabled firebase admin

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,10 @@
-import firebase from 'firebase'
+// import firebase from 'firebase'
+import admin from 'firebase-admin'
+import serviceAccount from '../firebase-credentials.json'
 
-const {
-  FIREBASE_API_KEY,
-  FIREBASE_AUTH_DOMAIN,
-  FIREBASE_DATABASE_URL,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUCKET,
-  FIREBASE_MESSAGING_SENDER_ID
-} = process.env
-
-const config = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  databaseURL: FIREBASE_DATABASE_URL,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID
-}
-
-firebase.initializeApp(config)
+const firebase = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
+})
 
 export default firebase
