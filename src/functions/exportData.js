490ed5d467560fbdf1c7e35ef45cc8db341fe76d
Daan Houweling <daan.gdh@gmail.com>
2017-09-05 08:25:48 +0200

    Do nothing when the token isn't defined

--- src/functions/exportData.js
+++ src/functions/exportData.js
@@ -7,6 +7,15 @@ const exportData = () => async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false
 
   const { token } = event.pathParameters
+  if (!token) {
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Done'
+      })
+    })
+  }
+
   const userId = await getUserIdByToken(token)
   if (userId === null) {
     return callback(null, {
