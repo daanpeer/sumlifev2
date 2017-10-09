3b8119b5292ec6c175b32acb7325c3bd7eb50789
Daan Houweling <daan.gdh@gmail.com>
2017-10-09 18:23:37 +0200

    Use admin version of firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import admin from 'firebase-admin'
 
-let serviceAccount
-if (process.env.NODE_ENV === 'production') {
-  serviceAccount = require('../firebase-credentials.prod.json')
-} else {
-  serviceAccount = require('../firebase-credentials.dev.json')
-}
+// let serviceAccount
+// if (process.env.NODE_ENV === 'production') {
+  // serviceAccount = require('../firebase-credentials.prod.json')
+// } else {
+const serviceAccount = require('../firebase-credentials.prod.json')
+// }
 
 const firebase = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
