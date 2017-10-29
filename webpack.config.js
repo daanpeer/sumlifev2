66ebfb9aa2a96b4fe32f732e8f3e4144ca934b77
Daan Houweling <daan.gdh@gmail.com>
2017-10-29 18:00:44 +0100

    Added minifcation

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,17 @@ const path = require('path')
 const { RUN_ENV } = process.env
 const nodeExternals = require('webpack-node-externals')
 const webpack = require('webpack')
+const MinifyPlugin = require('babel-minify-webpack-plugin')
 
 const plugins = []
 plugins.push(new webpack.DefinePlugin({
-  'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+  'process.env.RUN_ENV': JSON.stringify(RUN_ENV || 'local')
 }))
+plugins.push(new MinifyPlugin())
 
 const config = {
   entry: [
-    RUN_ENV === 'development' ? './local.js' : './index.js'
+    RUN_ENV === 'local' ? './local.js' : './index.js'
   ],
 
   devtool: 'sourcemap',
@@ -19,15 +21,13 @@ const config = {
   module: {
     rules: [{
       test: /\.js$/,
-      loader: 'babel-loader',
-      include: __dirname,
-      exclude: /node_modules/
+      loader: 'babel-loader'
     }]
   },
   plugins
 }
 
-if (RUN_ENV === 'development') {
+if (RUN_ENV === 'local') {
   config.output = {
     filename: 'build.js',
     path: path.join(__dirname, './dist')
