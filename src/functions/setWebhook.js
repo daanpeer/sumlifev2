6bdba1edf5be9ea164d2216f6dae658376e99cac
Daan Houweling <daan.gdh@gmail.com>
2017-11-11 16:57:53 +0100

    Fixed async function

--- src/functions/setWebhook.js
+++ src/functions/setWebhook.js
@@ -1,7 +1,7 @@
-const setWebhook = (bot) => function (event, context) {
+const setWebhook = (bot) => async (event, context) => {
   const url = `${process.env.BOT_WEBHOOK_URL}/${process.env.BOT_TOKEN}`
 
-  bot.telegram.setWebhook(url, null, 5, 5)
+  await bot.telegram.setWebhook(url, null, 5, 5)
 }
 
 export default setWebhook
