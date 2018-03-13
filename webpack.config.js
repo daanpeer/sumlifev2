1de0cb2a70c544c4541956c9e8f26ae0964417d6
Daan Houweling <daan.gdh@gmail.com>
2018-03-13 23:12:23 +0100

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,20 @@ const { RUN_ENV, NODE_ENV } = process.env
 const nodeExternals = require('webpack-node-externals')
 const webpack = require('webpack')
 const slsw = require('serverless-webpack')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 const plugins = []
 plugins.push(new webpack.DefinePlugin({
   'process.env.RUN_ENV': JSON.stringify(RUN_ENV || 'local'),
   'process.env.NODE_ENV': JSON.stringify(NODE_ENV || 'development'),
 }))
-plugins.push(new UglifyJSPlugin())
 
 const config = {
   entry: RUN_ENV === 'local' ? './local.js' : slsw.lib.entries,
-
+  mode: NODE_ENV,
+  optimization: {
+    minimize: false
+  },
   devtool: 'sourcemap',
   externals: [nodeExternals()],
   target: 'node',
