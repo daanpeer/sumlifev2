9822560de618fb0b7e7db369dc11a9de4f6cc208
Daan Houweling <daan.gdh@gmail.com>
2018-03-15 19:04:12 +0100

    Add prettier

--- src/commands/answerOpenQuestion.js
+++ src/commands/answerOpenQuestion.js
@@ -1,24 +1,17 @@
-import {
-  clearCommandState,
-  storeAnswerByToday
-} from '../queries'
+import { clearCommandState, storeAnswerByToday } from '../queries'
 
 const editQuestion = async (cmd, ctx, next) => {
   const message = ctx.message.text
   switch (cmd.state.step) {
     case 1: {
       await clearCommandState()
-      const {
-        questionId,
-        userId
-      } = cmd.state
+      const { questionId, userId } = cmd.state
       storeAnswerByToday(userId, questionId, message)
-      ctx.reply('I\'ve succesfully stored your answer 😃')
+      ctx.reply("I've succesfully stored your answer 😃")
       return next()
     }
     default:
       return next()
   }
 }
-
 export default editQuestion
